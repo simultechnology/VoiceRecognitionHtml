5c88d490785af77b04f4233f92d5d337433a38fd
t_ishikawa <tissi0708@gmail.com>
2019-03-29 01:46:08 +0900

    レイアウト調整

--- src/js/voice-detect.js
+++ src/js/voice-detect.js
@@ -9,8 +9,16 @@ let audioContext
 let mediaRecorder
 let gumStream
 
-const stopButton = document.getElementById('stop')
 const startButton = document.getElementById('start')
+const stopButton = document.getElementById('stop')
+
+startButton.addEventListener('click', () => {
+  document.getElementById('result').innerText = 'recording...'
+  navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+    .then(handleSuccess)
+  startButton.disabled = true
+  stopButton.disabled = false
+})
 
 stopButton.addEventListener('click', () => {
   // shouldStop = true
@@ -18,11 +26,8 @@ stopButton.addEventListener('click', () => {
   gumStream.getAudioTracks()[0].stop()
   createDownloadLink()
   mediaRecorder.clear()
-})
-
-startButton.addEventListener('click', () => {
-  navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-    .then(handleSuccess)
+  startButton.disabled = false
+  stopButton.disabled = true
 })
 
 const handleSuccess = (stream) => {
@@ -35,19 +40,45 @@ const handleSuccess = (stream) => {
 
 function createDownloadLink () {
   mediaRecorder && mediaRecorder.exportWAV(blob => {
-    const recordingsList = document.getElementById('recordingslist')
-    const url = URL.createObjectURL(blob)
-    const li = document.createElement('li')
-    const au = document.createElement('audio')
-    const hf = document.createElement('a')
-
-    au.controls = true
-    au.src = url
-    hf.href = url
-    hf.download = new Date().toISOString() + '.wav'
-    hf.innerHTML = hf.download
-    li.appendChild(au)
-    li.appendChild(hf)
-    recordingsList.appendChild(li)
+    console.log(blob)
+    document.getElementById('result').innerText = 'processing...'
+    const reader = new FileReader
+    reader.onload = () => {
+      const soundFile64 = reader.result.replace(/^data:audio\/(wav|mp3);base64,/, '')
+      console.log(soundFile64)
+      const data = new FormData()
+      data.append('voice-data', soundFile64)
+      const oReq = new XMLHttpRequest()
+      // oReq.open('POST', 'http://localhost:5000', true);
+      oReq.open('POST', 'https://rrtzxtv4ck.execute-api.ap-northeast-1.amazonaws.com/dev', true);
+      oReq.onload = (oEvent) => {
+        const responseText = oEvent.target.responseText
+        console.log(responseText);
+        let resultArea = document.getElementById('result')
+        if (responseText === 'MA') {
+          resultArea.innerText = 'it\'s a male voice'
+        } else if (responseText === 'FE') {
+          resultArea.innerText = 'it\'s a female voice'
+        } else {
+          resultArea.innerText = 'difficult to detect'
+        }
+      }
+      oReq.send(data)
+    }
+    reader.readAsDataURL(blob);
+    // const recordingsList = document.getElementById('recordingslist')
+    // const url = URL.createObjectURL(blob)
+    // const li = document.createElement('li')
+    // const au = document.createElement('audio')
+    // const hf = document.createElement('a')
+    //
+    // au.controls = true
+    // au.src = url
+    // hf.href = url
+    // hf.download = new Date().toISOString() + '.wav'
+    // hf.innerHTML = hf.download
+    // li.appendChild(au)
+    // li.appendChild(hf)
+    // recordingsList.appendChild(li)
   })
 }
